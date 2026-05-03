ff4f5c1519e0c2b9d0a201d21e99195cf5785379
Tammy-Pham <tptpdn@gmail.com>
2026-05-03 17:09:04 -0400

    implement java swing into here
    should work only issue might be is import, but maybe that's just a me thing, I did test this out in one class with the visualOutput.java file and it does print a chess board

--- App.java
+++ App.java
@@ -3,16 +3,24 @@
 
 package ser120.ChessProject3;
 
-import ser120.ChessProject3.GameManager;
-
+//import ser120.ChessProject3.GameManager;
+import javax.swing.*;
+import java.awt.*;
+import SER120.ChessProject3.VisualOutput;
 public class App {
 	//has the main method
 	
-    public static void main(String[] args) {
-        System.out.println("~~~ Chess App Open ~~~");
+   public static void main(String[] args) {
+		//prints the board
+		SwingUtilities.invokeLater(() -> {
+			VisualOutput window = new VisualOutput();
+			window.printBoard();
+		});
+		
+		/*System.out.println("~~~ Chess App Open ~~~");
         GameManager gm = new GameManager();
         gm.runGame();
         
-        System.out.println("~~~ Chess App Close ~~~");
-    }
+        System.out.println("~~~ Chess App Close ~~~");*/
+	}
 }
