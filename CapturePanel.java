4bc369ac8c0de6d7eea6d2712c420b45d0b3c976
Tammy-Pham <tptpdn@gmail.com>
2026-05-08 16:24:35 -0400

    Update CapturePanel to manage captured pieces list
    Refactored CapturePanel to use a list for captured pieces and updated painting logic.

--- CapturePanel.java
+++ CapturePanel.java
@@ -1,15 +1,11 @@
-package SER120.ChessProject3;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import javax.swing.JPanel;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.RenderingHints;
 
 public class CapturePanel extends JPanel {
 	
-	
-    private String blackCaptures = "BC ";
+	private List<Piece> capturedpieces = new ArrayList <>();
+   
 
     public CapturePanel() {
 		//if we want another panel I could just add another panel class for either team 
@@ -18,21 +14,35 @@ public class CapturePanel extends JPanel {
     }
     
     //this method prints out captured for string(but words get but out if added);
-	public void checkCapture() { blackCaptures = "bc: "; repaint(); }
+	public void checkCapture(Piece piece) { 
+			capturedpieces.add(piece);
+			repaint();
+		}
+	//need something for prameters to transfer information to here to display info
 
     @Override
     protected void paintComponent(Graphics g0) {
         super.paintComponent(g0);
         Graphics2D g = (Graphics2D) g0.create();
         g.setFont(new Font("Monospaced", Font.BOLD, 14));
-
+		 g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+		 g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
         g.setColor(new Color(0xE8C08C));
         //means black captures
-        g.drawString(blackCaptures, 12, 21);
-		 //going to need to find a way to print the pieces vertically 
-
-        g.setColor(new Color(0xFF3860));
         java.awt.FontMetrics fm = g.getFontMetrics();
+        g.translate(getWidth()/2.0,getHeight()/2.0);
+        g.rotate(-Math.PI/2);
+        //uses the array list that stores captured peices and should print the peices 
+        //to the side bar
+        for(int i = 0; i < capturedpieces.size();i++){
+			g.drawString(capturedpieces.get(i).getVisual(),4,fm.getAscent() + i * fm.getHeight());
+		}
+
+        //going to need to find a way to print the pieces vertically 
+        
+        //working on print the visuals on the side bar 
+       
+
         g.dispose();
     }
 }
