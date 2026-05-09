dbd21c3ae709fb80f11a0047b41fb2e0c49fe90a
emersonmedici <emersonmedici@gmail.com>
2026-05-08 22:33:52 -0400

    Add files via upload

--- OtherCaptureStatusPanel.java
+++ OtherCaptureStatusPanel.java
@@ -1,4 +1,4 @@
-package SER120.ChessProject3;
+package ser120.ChessProject4;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -11,7 +11,8 @@ import java.awt.RenderingHints;
 
 public class OtherCaptureStatusPanel extends JPanel {
 	
-	private List<Piece> capturedpieces = new ArrayList <>();
+	
+    private List<Piece> capturedpieces = new ArrayList <>();
 
     public OtherCaptureStatusPanel() {
 		//if we want another panel I could just add another panel class for either team 
@@ -20,19 +21,37 @@ public class OtherCaptureStatusPanel extends JPanel {
     }
     
     //this method prints out captured for string(but words get but out if added);
-	public void checkCapture(Piece piece) { 
-		if(piece == null) return;
+			public void checkCapture(Piece piece) { 
+			if(piece == null) return;
 		System.out.println("before capture: " + capturedpieces.size());
 			capturedpieces.add(piece);
 		System.out.println("black Captured: " + capturedpieces.size());
 			repaint();
-		}
-	//need something for prameters to transfer information to here to display info
+			}
 
     @Override
     protected void paintComponent(Graphics g0) {
-        super.paintComponent(g0);
-        System.out.println("checkCapture called, list size before: " + capturedpieces.size());
+        /*super.paintComponent(g0);
+        Graphics2D g = (Graphics2D) g0.create();
+        g.setFont(new Font("Monospaced", Font.BOLD, 14));
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+         //going to need to find a way to print the pieces vertically 
+			g.setColor(new Color(0xE8C08C));
+        //means black captures
+        java.awt.FontMetrics fm = g.getFontMetrics();
+        //g.translate(getWidth()/2.0,getHeight()/2.0);
+        //g.rotate(-Math.PI/2);
+        //uses the array list that stores captured peices and should print the peices 
+        //to the side bar
+        for(int i = 0; i < capturedpieces.size();i++){
+		g.drawString(capturedpieces.get(i).getVisual(),4,fm.getHeight() + i * fm.getHeight());
+
+		revalidate();
+        g.dispose();
+		}*/
+		
+		super.paintComponent(g0);
         Graphics2D g = (Graphics2D) g0.create();
         g.setFont(new Font("Monospaced", Font.BOLD, 14));
 		 g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
